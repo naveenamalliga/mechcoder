9a1aa2ad65504f860fad430ac6cd9f948d882a21
naveenamalliga <35651865+naveenamalliga@users.noreply.github.com>
2018-03-01 21:39:09 +0530

    Update and rename countnotspace.py to countword.py

--- countnotspace.py
+++
@@ -1,5 +0,0 @@
-a=str(input("enter the string")).split
-c=0
-for x in a
- c=c+1
-print("no. of character in string",c)
---
+++ countword.py
@@ -0,0 +1,3 @@
+a=str(input("enter the string")).split()
+c=len(a)
+print("no. of character in string",c)
